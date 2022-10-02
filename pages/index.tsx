f2b3febaf01d09492844b18195ae1f18b7bbefc3
Denis <denis@gmail.com>
2022-10-02 21:44:04 +0300

    Осталась валидация формы

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Htag } from "../components/Htag/Htag";
-
-export default function Home(): JSX.Element {
-  return (
-    <>
-      <Htag tag="h1">Выберите курс в разделе курсы</Htag>
-    </>
-  );
-}
