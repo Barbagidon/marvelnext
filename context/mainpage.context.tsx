b8287a391961cbba7b5ecef0811716a6f213f527
Denis <denis@gmail.com>
2022-10-04 13:57:41 +0300

    Приложуха готова

--- context/mainpage.context.tsx
+++ context/mainpage.context.tsx
@@ -21,6 +21,8 @@ export const MainPageContext = createContext<IMainPageContext>({
     description:
       "As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate.  He's self-assured, and he would never, ever stop fighting for a worthwhile cause.",
     id: 1009664,
+    homepage: "",
+    wiki: "",
   },
   charactersInfoForList: [],
   arrWithNumberPages: [],
